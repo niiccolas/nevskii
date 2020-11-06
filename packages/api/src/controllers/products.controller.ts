4ffab9b8ef4bf97bb68e49ad4edcf454b834749f
niiccolas <40167702+niiccolas@users.noreply.github.com>
2020-11-06 12:35:12 +0100

    feat(api#4): add pagination & sorting to /products endpoint

--- packages/api/src/controllers/products.controller.ts
+++ packages/api/src/controllers/products.controller.ts
@@ -7,55 +7,60 @@ export const getProducts = async (
   res: Response,
 ): Promise<Response> => {
   try {
-    const DEFAULT_TAKE = 10;
-    const DEFAULT_SKIP = 1;
-    const { title, size, media, page, order } = req.query;
+    const DEFAULT_ITEMS_PER_PAGE = 10;
+    const DEFAULT_PAGE = 0; // DML initializes first productId to 0
+    enum OrderBy {
+      price = 'price',
+      title = 'title',
+      createdAt = 'createdAt',
+    }
+
+    const {
+      title: titleQuery,
+      size: sizeQuery,
+      page: pageQuery,
+      order: orderQuery,
+      orderBy: orderByQuery,
+    } = req.query;
 
     let query = getRepository(Products)
       .createQueryBuilder('p')
-      // .leftJoinAndMapOne('p.mediaType', 'p.idMediaType', 'mt')
-      // .leftJoinAndMapOne('p.availability', 'p.idAvailability', 'av')
-      // .leftJoinAndMapOne('p.authors', 'p.productAuthors', 'pau')
-      .take(size ? Number(size) : DEFAULT_TAKE)
-      .skip(
-        page && Number(page) > 1
-          ? (Number(page) - 1) * (Number(size) || DEFAULT_TAKE)
-          : DEFAULT_SKIP,
-      );
+      .leftJoinAndMapOne('p.authors', 'p.productAuthors', 'pau')
+      .leftJoinAndMapOne('p.mediaType', 'p.idMediaType', 'mt')
+      .leftJoinAndMapOne('p.availability', 'p.idAvailability', 'pav');
 
-    if (order && typeof order === 'string') {
-      let sort: string;
-      const sortOrder: 'ASC' | 'DESC' | undefined =
-        order[0] === 'A' ? 'ASC' : 'DESC';
+    if (titleQuery)
+      query = query
+        .where('p.title ILIKE :title', { title: '%' + titleQuery + '%' })
+        .orWhere('p.titleOriginal ILIKE :title', {
+          title: '%' + titleQuery + '%',
+        });
 
-      switch (order.slice(1)) {
-        case 'year':
-          sort = 'p.productionYear';
-          break;
-        case 'price':
-          sort = 'p.price';
-          break;
-        case 'title':
-          sort = 'p.title';
-          break;
-        default:
-          sort = 'p.productionYear';
-          break;
-      }
-
-      query = query.orderBy(sort, sortOrder);
+    if (orderQuery && orderByQuery) {
+      const order =
+        orderQuery && `${orderQuery}`.toUpperCase() == 'DESC' ? 'DESC' : 'ASC';
+      const orderBy = (<any>OrderBy)[`${req.query.orderBy}`]
+        ? orderByQuery
+        : OrderBy.createdAt;
+      query.orderBy(`p.${orderBy}`, order);
     }
 
-    if (title)
-      query = query
-        .where('p.title ILIKE :title', { title: '%' + title + '%' })
-        .orWhere('p.titleOriginal ILIKE :title', { title: '%' + title + '%' });
+    const takeItems: number = sizeQuery
+      ? Number(sizeQuery)
+      : DEFAULT_ITEMS_PER_PAGE;
+    const skipItems: number =
+      pageQuery && Number(pageQuery) > 1
+        ? (Number(pageQuery) - 1) * takeItems
+        : DEFAULT_PAGE;
+    query.take(takeItems).skip(skipItems);
 
-    if (media)
-      query = query.andWhere('mt.idMediaType = :media', { media: media });
-
-    const [users, count] = await query.getManyAndCount();
-    return res.json({ itemsCount: count, items: users });
+    const [products, count] = await query.getManyAndCount();
+    return res.json({
+      page: Number(pageQuery) || DEFAULT_PAGE,
+      itemsPerPage: takeItems || DEFAULT_ITEMS_PER_PAGE,
+      itemsTotal: count,
+      items: products,
+    });
   } catch (error) {
     return res.json(error);
   }
