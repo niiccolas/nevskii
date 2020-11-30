2832519155e5dc86860ea78b82edb5e92dda5e45
niiccolas <40167702+niiccolas@users.noreply.github.com>
2020-11-30 17:58:57 +0100

    feat(WEB#8): enhance formatting helper functions

--- packages/web/src/utils/formatting.ts
+++ packages/web/src/utils/formatting.ts
@@ -1,2 +1,20 @@
-export const formatPrice = (price: string): string =>
-  (parseInt(price, 10) / 100).toFixed(2);
+/**
+ * Format price values coming from database and appends currency
+ *
+ * @param price - price in cents and typed as a string
+ * @param currency - default to EUROS
+ */
+export const formatPrice = (price: string, currency: '€' = '€'): string =>
+  (parseInt(price, 10) / 100).toFixed(2) + '\u00a0' + currency;
+
+/**
+ * Format comma-separated values from database
+ *
+ * @param list - A database comma-separated aggregate
+ * @param separator - The desired output value separator
+ */
+export const formatValuesList = (list: string, separator = ', '): string =>
+  list
+    .split(';')
+    .map(value => value[0].toUpperCase() + value.slice(1))
+    .join(separator);
