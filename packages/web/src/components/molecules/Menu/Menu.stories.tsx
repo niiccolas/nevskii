59b868ae8bfdd1f13969f50d08014e118c5e174d
niiccolas <40167702+niiccolas@users.noreply.github.com>
2020-12-30 12:31:28 +0100

    feat(WEB#7): add Menu to Navbar

--- packages/web/src/components/molecules/Menu/Menu.stories.tsx
+++ packages/web/src/components/molecules/Menu/Menu.stories.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import { Menu, MenuProps } from '@Molecules';
+import { withNextRouter } from 'storybook-addon-next-router';
 
 export default {
   title: 'Molecules/Menu',
   component: Menu,
+  decorators: [withNextRouter],
 } as Meta;
 
 const Template: Story<MenuProps> = args => <Menu {...args} />;
--- packages/web/src/components/molecules/Menu/Menu.tsx
+++ packages/web/src/components/molecules/Menu/Menu.tsx
@@ -1,10 +1,17 @@
+import Link from 'next/link';
+import { useState } from 'react';
+import { FaShoppingCart, FaBox, FaBars } from 'react-icons/fa';
+import { BiHelpCircle } from 'react-icons/bi';
+
+import { Button } from '@Atoms';
+
 import './Menu.scss';
 
 export type MenuProps = {
-  items: MenuItemProps[];
+  items: MenuItem[];
 };
 
-export type MenuItemProps = {
+export type MenuItem = {
   label: string;
   href: string;
   id: string;
@@ -14,23 +21,68 @@ export type MenuItemProps = {
  * Menu
  */
 export const Menu: React.FC<MenuProps> = ({ items }) => {
+  const [active, setActive] = useState(false);
+
+  // Close when viewport is resized
+  document.addEventListener('resize', () => setActive(false));
+  // When Menu is active, disable scrolling on body
+  document && active
+    ? (document.body.className = 'Menu__freezeBody')
+    : (document.body.className = '');
+
+  const handleClick = () => setActive(!active);
+
   return (
     <div className="Menu">
-      <input type="checkbox" className="Menu__toggler" />
-      <div className="Menu__hamburger">
-        <div></div>
-      </div>
-      <div className="Menu__items">
-        <div>
-          <ul>
-            {items.map(({ label, href, id }) => (
-              <li key={id}>
-                <a href={href}>{label}</a>
-              </li>
-            ))}
-          </ul>
+      <div
+        className={['Menu__items', active && 'Menu__items--active'].join(' ')}
+      >
+        {items.map(({ label, href, id }) => (
+          <Link key={id} href={href}>
+            {label}
+          </Link>
+        ))}
+
+        <div className="Menu__CTA">
+          <Button label="Signup" type="button" primary />
+          <Button label="Log-in" type="button" />
         </div>
+
+        <ul className="Menu__footer">
+          <Link href="/cart">
+            <li>
+              <span className="Menu__footerIcon">
+                <FaShoppingCart />
+              </span>
+              <span>Cart</span>
+            </li>
+          </Link>
+          <Link href="#">
+            <li>
+              <span className="Menu__footerIcon">
+                <FaBox />
+              </span>
+              <span>Orders</span>
+            </li>
+          </Link>
+          <Link href="#">
+            <li>
+              <span className="Menu__footerIcon">
+                <BiHelpCircle />
+              </span>
+              <span>FAQ</span>
+            </li>
+          </Link>
+        </ul>
       </div>
+
+      <div
+        className={['Menu__overlay', active && 'Menu__overlay--active'].join(
+          ' ',
+        )}
+        onClick={() => active && setActive(!active)}
+      ></div>
+      <FaBars onClick={handleClick} className="Menu__toggle" />
     </div>
   );
 };
